e2a3fd2edd449026aa17ba0ab61f9cbdae1eb719
ice_iix <ice_ix@protonmail.ch>
2019-05-28 19:15:44 -0700

    Add example

---
+++ examples/hello/src/main.rs
@@ -0,0 +1,39 @@
+use std::io::{Read, Write};
+use log::info;
+use cfg_if::cfg_if;
+
+cfg_if! {
+    if #[cfg(target_arch = "wasm32")] {
+        use std_or_web::fs;
+    } else {
+        use std::fs;
+    }
+}
+
+struct Logger {}
+impl log::Log for Logger {
+    fn enabled(&self, _: &log::Metadata) -> bool { true }
+    fn flush(&self) { }
+
+    fn log(&self, record: &log::Record) {
+        println!("{}", record.args());
+    }
+
+}
+
+fn main() {
+    log::set_boxed_logger(Box::new(Logger{})).unwrap();
+    log::set_max_level(log::LevelFilter::Trace);
+
+    info!("creating");
+    let mut f = fs::File::create("hello.txt").unwrap();
+    info!("writing");
+    f.write_all(b"Hello, world!").unwrap();
+
+    info!("opening");
+    let mut g = fs::File::open("hello.txt").unwrap();
+    let mut contents = String::new();
+    info!("reading");
+    g.read_to_string(&mut contents).unwrap();
+    info!("read data = {:?}", contents);
+}
